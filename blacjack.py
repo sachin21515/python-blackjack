e0d5980b2d3c0efbfc5a7a57805583817a8493ce
sachin21515 <82361979+sachin21515@users.noreply.github.com>
2022-10-10 20:42:44 -0400

    Update blacjack.py
    reduced some lines of code

--- blacjack.py
+++ blacjack.py
@@ -2,31 +2,24 @@ import random
 import os
 import time
 def play_game():
-  play = input("Do you want to play blackjack? Type 'y' or 'n'")
-    
+  play = input("Do you want to play blackjack? Type 'y' or 'n'") 
   card_list = [1,2,3,4,5,6,7,8,9,10,11,12,13]
-  
   #player card will be stored in player list
   player = [] 
   #computer card will be stored in computer list
   computer = []
-  for i in range(1,3):
-    card = card_list[random.randint(1,13)-1]
-    if card>10:
-      player.append(10)
-    elif card==1:
-      player.append(11)
-    else:
-      player.append(card)
-      
-  for i in range(1,3):
-    card = card_list[random.randint(1,13)-1]
-    if card>10:
-      computer.append(10)
-    elif card==1:
-      computer.append(11)  
-    else:
-      computer.append(card)
+  def intial_cards(game_players):
+    for i in range(1,3):
+      card = card_list[random.randint(1,13)-1]
+      if card>10:
+        game_players.append(10)
+      elif card==1:
+        game_players.append(11)
+      else:
+        game_players.append(card)
+
+  intial_cards(player) 
+  intial_cards(computer)
   
   def start():
     #should choose will be true until player does not want to fetch 
@@ -36,23 +29,26 @@ def play_game():
     print(f"Computer's first card: {computer[0]}")
   
     while should_choose :
-    
+      def new_card(game_players):
+          new_card = card_list[random.randint(1,13)-1]
+          if new_card >10:
+            game_players.append(10)
+          #if new_card is 1 then check for the sum of all cards
+          elif new_card==1:
+            if sum(game_players)<=10:
+            #if sum <10 then ace value is 11
+              game_players.append(11)
+            else:
+            #otherwise ace value is 1
+              game_players.append(1)
+          else:
+          #if new_card is between 2 and 10 then append as it is
+            game_players.append(new_card)
+  
       want_card = input('type y to get new card or n to pass ')
       if want_card == 'y':
-        new_card = card_list[random.randint(1,13)-1]
-        if new_card >10:
-          player.append(10)
-          #if new_card is 1 then check for the sum of all cards
-        elif new_card==1:
-          if sum(player)<=10:
-            #if sum <10 then ace value is 11
-            player.append(11)
-          else:
-            #otherwise ace value is 1
-            player.append(1)
-        else:
-          #if new_card is between 2 and 10 then append as it is
-          player.append(new_card)
+
+        new_card(player)
         print(f"Your cards: {player}") 
         if sum(player)>21:
           #if sum is >21 the break out of the while loop
@@ -65,16 +61,8 @@ def play_game():
         # if it is <17 the will add new card
         def computer_hand():
           if sum(computer)<17:
-            computer_new_card = card_list[random.randint(1,13)-1]
-            if computer_new_card ==1:
-              if sum(computer)<=10:
-                computer.append(11)
-              else:
-                computer.append(1)
-            elif computer_new_card >10:
-              computer.append(10)
-            else:
-              computer.append(computer_new_card)
+            new_card(computer)
+            
             computer_hand()
           else:
             print(f"Computer's final hand: {computer}")
